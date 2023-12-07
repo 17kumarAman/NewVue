75daf86ea7a079b52d57606b706443ddac4387b1
17kumarAman <ak3722032@gmail.com>
2023-12-07 16:41:16 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue'
+import child from './components/Child.vue'
+import slots from './components/Namedslot.vue'
+  
+const message = ref('hello')
+let arr = ['hello world', 'Namastee duniya ']
+
+function call(e){
+    alert(e)
+}
+
+</script>
+
+<template>
+<child v-model="message" :data="arr" @events="(e)=>call(e)"/> {{ message }}
+<slots>
+  <template #header>
+      <h1 >Names slots starts here </h1>
+    </template>
+
+    <template #default>
+      <p>A paragraph to default view</p>
+
+    </template>
+
+    <template #footer>
+      <h3>Names slots Ends here</h3>
+    </template>
+</slots>
+
+<button @click="alert('hello')">Clickme</button>
+
+
+</template>
+
+
---
+++ src/components/Child.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps(['data'])
+defineEmits(['events'])
+
+
+</script>
+
+<template>
+
+    <div>
+        <h4 v-for="e in data">
+            {{ e }}
+            <button @click="$emit('events', e)">Emits</button>
+        
+        </h4>
+    </div>
+</template>
